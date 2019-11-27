91100e4122571481ec640edecbffb62c87faa32b
Andy <35195301+Andy53@users.noreply.github.com>
2019-11-27 14:13:21 -0800

    Update Display_Output.cs

--- ERC/Display_Output.cs
+++ ERC/Display_Output.cs
@@ -1955,7 +1955,7 @@ namespace ERC
             {
                 throw asmResult.Error;
             }
-            string opcodes = BitConverter.ToString(asmResult.ReturnValue).Replace("-", "");
+            string opcodes = BitConverter.ToString(asmResult.ReturnValue).Replace("-", " ");
             opcodeArray = opcodes.Split(' ');
             return opcodeArray;
         }
