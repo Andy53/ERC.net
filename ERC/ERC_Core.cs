55f6e2e7d2adb700c59efa423b190aa3ec5fd85a
Andy <35195301+Andy53@users.noreply.github.com>
2020-02-21 07:53:10 -0800

    Documentation Updates

--- ERC/ERC_Core.cs
+++ ERC/ERC_Core.cs
@@ -329,6 +329,9 @@ namespace ERC
             PatternStandardPath = "";
             PatternExtendedPath = "";
             SystemErrorLogPath = Path.Combine(WorkingDirectory, "System_Error.LOG");
+            
+            ErcVersion = "ERC.Xdbg_32-1.1"; //Uncomment for 32bit.
+            //ErcVersion = "ERC.Xdbg_64-1.1"; //Uncomment for 32bit.
 
             bool configRead = false;
             while (configRead == false)
